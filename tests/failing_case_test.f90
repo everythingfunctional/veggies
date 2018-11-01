a5571841dc4f643a169281a07f8b2276f1b4f60e
Brad Richardson <brichardson@structint.com>
2018-11-01 12:12:52 -0700

    test number of cases for failing test case

--- tests/failing_case_test.f90
+++ tests/failing_case_test.f90
@@ -11,7 +11,8 @@ contains
 
         test = given("a failing test case", &
                 [when("it is run", &
-                        [then("it knows it failed", checkCaseFails)])])
+                        [then("it knows it failed", checkCaseFails), &
+                        then("it still has 1 test case", checkNumCases)])])
     end function test_failing_case_behaviors
 
     function checkCaseFails() result(result_)
@@ -27,4 +28,18 @@ contains
         test_result = test_case%run()
         result_ = assertNot(test_result%passed())
     end function checkCaseFails
+
+    function checkNumCases() result(result_)
+        use example_cases_m, only: exampleFailingTestCase
+        use Vegetables_m, only: Result_t, TestCase_t, TestCaseResult_t, assertEquals
+
+        type(Result_t) :: result_
+
+        type(TestCase_t) :: test_case
+        type(TestCaseResult_t) :: test_result
+
+        test_case = exampleFailingTestCase()
+        test_result = test_case%run()
+        result_ = assertEquals(1, test_result%numCases())
+    end function checkNumCases
 end module failing_case_test
