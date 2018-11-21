a217b32e01c1ea818b243ad4ba47ac248b950d9a
Brad Richardson <brichardson@structint.com>
2018-11-21 13:15:17 -0800

    Finish the collection properties test

--- tests/collection_properties_test.f90
+++ tests/collection_properties_test.f90
@@ -11,7 +11,8 @@ contains
 
         test = describe("A test collection", &
                 [it("can tell how many tests it has", checkNumCases), &
-                it("includes the given description", checkCollectionTopDescription)])
+                it("includes the given description", checkCollectionTopDescription), &
+                it("includes the individual test descriptions", checkCollectionDescriptions)])
     end function test_collection_properties
 
     function checkNumCases() result(result_)
@@ -40,4 +41,24 @@ contains
         result_ = assertIncludes( &
                 EXAMPLE_COLLECTION_DESCRIPTION, test_collection%description())
     end function checkCollectionTopDescription
+
+    function checkCollectionDescriptions() result(result_)
+        use example_collections_m, only: &
+                examplePassingCollection, &
+                EXAMPLE_CASE_DESCRIPTION_1, &
+                EXAMPLE_CASE_DESCRIPTION_2
+        use Vegetables_m, only: &
+                Result_t, TestCollection_t, assertIncludes
+
+        type(Result_t) :: result_
+
+        type(TestCollection_t) :: test_collection
+
+        test_collection = examplePassingCollection()
+        result_ = &
+                assertIncludes( &
+                        EXAMPLE_CASE_DESCRIPTION_1, test_collection%description()) &
+                .and.assertIncludes( &
+                        EXAMPLE_CASE_DESCRIPTION_2, test_collection%description())
+    end function checkCollectionDescriptions
 end module collection_properties_test
