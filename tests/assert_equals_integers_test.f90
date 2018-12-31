7f9f419a7f4eaa4f2f58e3f9657791609cda9c64
Brad Richardson <brichardson@structint.com>
2018-12-31 11:45:25 -0800

    Add tests for assertEquals with integers

---
+++ tests/assert_equals_integers_test.f90
@@ -0,0 +1,40 @@
+module assert_equals_integers_test
+    implicit none
+    private
+
+    public :: test_assert_equals_integers
+contains
+    function test_assert_equals_integers() result(tests)
+        use Vegetables_m, only: TestItem_t, describe, it
+
+        type(TestItem_t) :: tests
+
+        tests = describe("assertEquals with integers", &
+                [it("passes with the same integer", checkPassForSameInteger), &
+                it("fails with different integers", checkFailForDifferentIntegers)])
+    end function test_assert_equals_integers
+
+    function checkPassForSameInteger() result(result_)
+        use Vegetables_m, only: Result_t, assertEquals, assertThat
+
+        type(Result_t) :: result_
+
+        type(Result_t) :: example_result
+
+        example_result = assertEquals(1, 1)
+
+        result_ = assertThat(example_result%passed(), "It passed", "It didn't pass")
+    end function checkPassForSameInteger
+
+    function checkFailForDifferentIntegers() result(result_)
+        use Vegetables_m, only: Result_t, assertEquals, assertNot
+
+        type(Result_t) :: result_
+
+        type(Result_t) :: example_result
+
+        example_result = assertEquals(1, 2)
+
+        result_ = assertNot(example_result%passed(), "It didn't pass", "It passed")
+    end function checkFailForDifferentIntegers
+end module assert_equals_integers_test
