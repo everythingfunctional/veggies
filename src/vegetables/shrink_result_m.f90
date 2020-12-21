bbd80cbdbc328dd533ff97cc9a561002ab518104
Brad Richardson <everythingfunctional@protonmail.com>
2020-12-21 15:00:24 -0600

    refactor: explicitly assign components of shrink_result constructors

--- src/vegetables/shrink_result_m.f90
+++ src/vegetables/shrink_result_m.f90
@@ -10,26 +10,19 @@ module vegetables_shrink_result_m
         logical :: simplest
     end type
 contains
-    pure function shrink_result(value_, simplest)
-        class(input_t), intent(in) :: value_
-        logical, intent(in) :: simplest
-        type(shrink_result_t) :: shrink_result
-
-        allocate(shrink_result%input, source = value_)
-        shrink_result%simplest = simplest
-    end function
-
     pure function shrunk_value(value_)
         class(input_t), intent(in) :: value_
         type(shrink_result_t) :: shrunk_value
 
-        shrunk_value = shrink_result(value_, .false.)
+        allocate(shrunk_value%input, source = value_)
+        shrunk_value%simplest = .false.
     end function
 
     pure function simplest_value(value_)
         class(input_t), intent(in) :: value_
         type(shrink_result_t) :: simplest_value
 
-        simplest_value = shrink_result(value_, .true.)
+        allocate(simplest_value%input, source = value_)
+        simplest_value%simplest = .true.
     end function
 end module
