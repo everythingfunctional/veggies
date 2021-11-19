071202dfb108a319ca987904af13415022b15c14
Brad Richardson <everythingfunctional@protonmail.com>
2021-11-19 16:19:20 -0600

    feat(assert_equals): for integer arrays

--- src/vegetables/assert_m.f90
+++ src/vegetables/assert_m.f90
@@ -4,6 +4,7 @@ module vegetables_assert_m
     use vegetables_assert_equals_double_precision_m, only: assert_equals
     use vegetables_assert_equals_double_precision_array_m, only: assert_equals
     use vegetables_assert_equals_integer_m, only: assert_equals
+    use vegetables_assert_equals_integer_array_m, only: assert_equals
     use vegetables_assert_equals_strings_m, only: assert_equals
     use vegetables_assert_equals_within_absolute_m, only: &
             assert_equals_within_absolute
